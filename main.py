0c0d6b08e84508fb1ca693037bf039381f14c714
OmarAbdelwahaab <omarabdelwahaab1000@gmail.com>
2026-08-02 21:07:01 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import uvicorn
+import uvicorn, requests
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
+from typing import Optional
 
 app = FastAPI()
 
 
 class TaskCreate(BaseModel):
     title: str
+    state: Optional[bool] = None 
+    
+    
+tid = 1
 
 tasks = [
     [1, "clean room", True],
@@ -16,7 +20,6 @@ tasks = [
 ]
 
 tid = 3
-
 
 @app.get("/")
 def Details():
@@ -46,5 +49,32 @@ def add_task(task: TaskCreate):
 
 
 
+@app.put("/tasks/:id")
+def update_task(task : TaskCreate ,tid : int):
+    
+    if tid > len(tasks) or tid == 0:
+            raise HTTPException(status_code=404)
+        
+    elif (task.title == "") or (task.state is None):
+        raise HTTPException(status_code=400, detail="Empty/Invalid body")
+        
+    else:
+        tid -= 1 
+        updatedTask = [tasks[tid][0], task.title, task.state]
+        tasks[tid] = updatedTask
+        return updatedTask
+
+@app.delete("/tasks/:id")
+def delete_task(tid: int):
+    
+    if tid > len(tasks) or tid == 0:
+            raise HTTPException(status_code=404, detail=f"Task {tid} not found")
+            
+    index = tid - 1
+    del tasks[index]        
+    raise HTTPException(status_code=204)
+  
+    
+    
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
