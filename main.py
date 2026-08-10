72211ce97047092cb3cb7370fea29d399b425230
OmarAbdelwahaab <omarabdelwahaab1000@gmail.com>
2026-08-10 16:08:51 +0300

    Stage 1: signup and login routes working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from supabase import create_client, Client
 from contextlib import asynccontextmanager
 from pathlib import Path
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 from typing import Optional
 
@@ -23,6 +23,8 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/")
 def read_root():
@@ -70,6 +72,10 @@ class TaskCreate(BaseModel):
 
 class SQLCommand(BaseModel):
     query: str
+
+class AuthCredentials(BaseModel):
+    email: str
+    password: str
 
 
 @app.get("/", description="Get some details")
@@ -185,8 +191,65 @@ def delete_task(id: int):
     conn.close()
     return Response(status_code=204)
   
+
+@app.post("/auth/signup", status_code=status.HTTP_201_CREATED)
+def signup(credentials: AuthCredentials):
+  
+    if not credentials.email.strip() or not credentials.password.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email and password are required"
+        )
+
+    try:
+        
+        response = supabase.auth.sign_up({
+            "email": credentials.email,
+            "password": credentials.password
+        })
+
+        if not response.user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Signup failed"
+            )
+
+
+        return response.user
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
     
-    
-    
+@app.post("/auth/login")
+def login(credentials: AuthCredentials):
+    if not credentials.email.strip() or not credentials.password.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email and password are required"
+        )
+
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email": credentials.email,
+            "password": credentials.password
+        })
+
+        return {
+            "access_token": response.session.access_token,
+            "refresh_token": response.session.refresh_token,
+            "token_type": "bearer"
+        }
+
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid login credentials"
+        )
+        
+        
+        
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
